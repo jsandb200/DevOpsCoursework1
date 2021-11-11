3eb18bd9b5a0bff69325190bf3f21cc5464055f5
jsandb200 <58029621+jsandb200@users.noreply.github.com>
2021-11-11 18:09:31 +0000

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-// Compile the java file.
+# Compile the java file.
 echo "Compiling..."
 javac Dec2Hex.java
 echo "Compile successful."
 
-// Test 1
+# Test 1
 echo "Beginning integer test (13)..."
 java Dec2Hex 13
 
-// Test 2
+# Test 2
 echo "Beginning letter test (F)..."
 java Dec2Hex F
 
-// Test 3
+# Test 3
 echo "Beginning inputless test..."
 java Dec2Hex
 
-// Test 4
+# Test 4
 echo "Beginning large integer test..."
 java Dec2Hex 1324
 
